a5173e7a8a8a59fa9c86ee8e3f032b282bfde21f
LeonardoGalarza <leonardofermingalarza@gmail.com>
2022-06-07 20:13:37 -0300

    filter_ok

--- lib/Ultrasonic_sensor/Ultrasonic_sensor.cpp
+++ lib/Ultrasonic_sensor/Ultrasonic_sensor.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
 #include "Ultrasonic_sensor.h"
 
-ultrasonicSensor::ultrasonicSensor(byte trigger, byte echo){
+ultrasonicSensor::ultrasonicSensor(byte trigger, byte echo, unsigned char sample){
     _trigger=trigger;
     _echo=echo;
+    _sample=sample;
     pinMode(_trigger,OUTPUT);
     pinMode(_echo,INPUT);
     digitalWrite(_trigger,false);
 }
-
 long ultrasonicSensor::distance(){
-    digitalWrite(_trigger,true);
-    delayMicroseconds(10);
-    digitalWrite(_trigger,false);
-
-    _timeEcho=pulseIn(_echo,true);
-    
-    return _timeEcho/59;
+    _timeEcho=0;
+    for (char i = 0; i <_sample; i++)
+    {
+        digitalWrite(_trigger,true);
+        delayMicroseconds(10);
+        digitalWrite(_trigger,false);
+        _timeEcho+=pulseIn(_echo,true)/59;
+        delay(10);
+    }
+    return _timeEcho/_sample;
 }
